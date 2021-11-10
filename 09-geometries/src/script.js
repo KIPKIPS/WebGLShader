b7b266ce7b403193b9ef296a4522b0a8a3ff0b86
KIPKIPS <2432719656@qq.com>
2021-11-10 16:51:40 +0800

    BufferGeometry和BufferAttribute使用

--- 09-geometries/src/script.js
+++ 09-geometries/src/script.js
@@ -3,13 +3,7 @@ import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 import ky from "kyouka";
 
-/**
- * Base
- */
-// Canvas
 const canvas = document.querySelector("canvas.webgl");
-
-// Scene
 const scene = new THREE.Scene();
 
 // Object
@@ -30,6 +24,7 @@ const scene = new THREE.Scene();
 const geometry = new THREE.BufferGeometry();
 // const positionsArray = new Float32Array([0, 0, 0, 0, 1, 0, 1, 0, 0]);
 const count = 50;
+//buffer geometry 顶点数组,三个一组为一个三维向量数据
 const positionsArray = new Float32Array(count * 3 * 3);
 for (let i = 0; i < count * 3 * 3; i++) {
     positionsArray[i] = ky.randomNumberInRange(-0.5, 0.5) * 4;
@@ -37,67 +32,44 @@ for (let i = 0; i < count * 3 * 3; i++) {
 const positionsAttribute = new THREE.BufferAttribute(positionsArray, 3);
 geometry.setAttribute("position", positionsAttribute);
 const material = new THREE.MeshBasicMaterial({
-  color: 0xff0000,
-  wireframe: true,
+    color: 0xff0000,
+    wireframe: true,
 });
 const mesh = new THREE.Mesh(geometry, material);
 scene.add(mesh);
-
 // Sizes
 const sizes = {
-  width: window.innerWidth,
-  height: window.innerHeight,
+    width: window.innerWidth,
+    height: window.innerHeight,
 };
-
 window.addEventListener("resize", () => {
-  // Update sizes
-  sizes.width = window.innerWidth;
-  sizes.height = window.innerHeight;
-
-  // Update camera
-  camera.aspect = sizes.width / sizes.height;
-  camera.updateProjectionMatrix();
-
-  // Update renderer
-  renderer.setSize(sizes.width, sizes.height);
-  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+    sizes.width = window.innerWidth;
+    sizes.height = window.innerHeight;
+    camera.aspect = sizes.width / sizes.height;
+    camera.updateProjectionMatrix();
+    renderer.setSize(sizes.width, sizes.height);
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 });
-
 // Camera
-const camera = new THREE.PerspectiveCamera(
-  75,
-  sizes.width / sizes.height,
-  0.1,
-  100
-);
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100);
 camera.position.z = 3;
 scene.add(camera);
-
 // Controls
 const controls = new OrbitControls(camera, canvas);
 controls.enableDamping = true;
-
 // Renderer
 const renderer = new THREE.WebGLRenderer({
-  canvas: canvas,
+    canvas: canvas,
 });
 renderer.setSize(sizes.width, sizes.height);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
-
 // Animate
 const clock = new THREE.Clock();
-
 const tick = () => {
-  const elapsedTime = clock.getElapsedTime();
-
-  // Update controls
-  controls.update();
-
-  // Render
-  renderer.render(scene, camera);
-
-  // Call tick again on the next frame
-  window.requestAnimationFrame(tick);
+    const elapsedTime = clock.getElapsedTime();
+    controls.update();
+    renderer.render(scene, camera);
+    window.requestAnimationFrame(tick);
 };
 
 tick();
