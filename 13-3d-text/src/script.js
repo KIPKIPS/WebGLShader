c9fc92defb70cd4dd77e5a257abbc3e45059e3ce
KIPKIPS <2432719656@qq.com>
2021-11-25 16:05:08 +0800

    Task P15 Shadow,Time 00:02:11

--- 13-3d-text/src/script.js
+++ 13-3d-text/src/script.js
@@ -17,7 +17,7 @@ const scene = new THREE.Scene()
  * Textures
  */
 const textureLoader = new THREE.TextureLoader()
-const matcapTexture = textureLoader.load('/textures/matcaps/8.png')
+const matcapTexture = textureLoader.load('/textures/matcaps/3.png')
 
 const sizes = {
     width: window.innerWidth,
@@ -62,6 +62,7 @@ controls.enableDamping = true
  */
 const renderer = new THREE.WebGLRenderer({
     canvas: canvas,
+    antialias: true,
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
@@ -93,26 +94,27 @@ fontLoader.load('/fonts/helvetiker_regular.typeface.json', font => {
         font,
         size: 0.5,
         height: 0.2,
-        curveSegments: 12,
+        curveSegments: 6,
         bevelEnabled: true,
         bevelThickness: 0.03,
         bevelSize: 0.02,
         bevelOffset: 0,
         bevelSegments: 5,
     })
-    const mat = new THREE.MeshMatcapMaterial({ matcap: matcapTexture })
+    const mat = new THREE.MeshMatcapMaterial({ matcap: matcapTexture, });
+    // mat.wireframe = true;
     const text = new THREE.Mesh(textGeo, mat)
-    scene.add(text)
-    textGeo.computeBoundingBox()
-        //   console.log(textGeometry.boundingBox);
-        //   textGeometry.translate(
-        //     -(textGeometry.boundingBox.max.x - 0.02) * 0.5,
-        //     -(textGeometry.boundingBox.max.y - 0.02) * 0.5,
-        //     -(textGeometry.boundingBox.max.z - 0.03) * 0.5
-        //   );
-    textGeo.center()
+    scene.add(text);
+    textGeo.computeBoundingBox();
+    console.log(textGeo.boundingBox);
+    // bevelSize 会影响x,y的坐标中心 bevelThickness会影响到z
+    textGeo.translate(-(textGeo.boundingBox.max.x - 0.02) * 0.5, -(textGeo.boundingBox.max.y - 0.02) * 0.5, -(textGeo.boundingBox.max.z - 0.03) * 0.5);
+    // textGeo.center();
 
-    const donutGeo = new THREE.TorusBufferGeometry(0.3, 0.2, 20, 45)
+    const donutGeo = new THREE.TorusBufferGeometry(0.3, 0.2, 20, 45);
+    const axesHelper = new THREE.AxesHelper();
+    scene.add(axesHelper);
+    console.time('dounts');
     for (let i = 0; i < 100; i++) {
         const donut = new THREE.Mesh(donutGeo, mat)
         scene.add(donut)
@@ -128,4 +130,5 @@ fontLoader.load('/fonts/helvetiker_regular.typeface.json', font => {
         const scale = ky.randomNumberInRange(0, 1)
         donut.scale.set(scale, scale, scale)
     }
+    console.timeEnd('dounts');
 })
